e31ec090b76f95af22bea748589162ff655b6124
1nfer.exe <InferKing@users.noreply.github.com>
2024-02-19 12:00:16 +0500

    added redraw btn and generate btn

--- graph.py
+++ graph.py
@@ -13,25 +13,20 @@ class GraphGUI(ctk.CTkToplevel):
         super().__init__(parent)
         ctk.set_appearance_mode(config.APPEARANCE_MODE)
         self.title(config.TEXT_APP_TITLE)
+        self.resizable(False, False)
         self.frame = tk.Frame(self)
         self.frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-        G = nx.DiGraph()
-        data = DataConverter().json_to_graph(FileManager().load_file_data(config.FILENAME_DATA))
-        G.add_edges_from([(edge[0], edge[1], {'capacity': capacity}) for edge, capacity in zip(data[0], data[1])])
-        pos = nx.spring_layout(G, k=len(data[1]))
+        self.data = DataConverter().json_to_graph(FileManager().load_file_data(config.FILENAME_DATA))
+
         self.fig = plt.figure()
-        self.ax = self.fig.add_subplot(111)
         self.canvas = FigureCanvasTkAgg(self.fig, self.frame)
 
-        nx.draw(G, pos, ax=self.ax, with_labels=True, node_size=700, node_color="skyblue", font_size=10,
-                font_color="black", edge_color='red')
-
-        nx.draw_networkx_edge_labels(G, pos, edge_labels={key: value for key, value in zip(data[0], data[1])})
-
-        self.ax.set_title(config.TEXT_APP_TITLE)
-        self.canvas.draw()
+        self.draw_graph()
         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+        self.btn_redraw = ctk.CTkButton(self.frame, text=config.TEXT_BTN_REDRAW, command=self.draw_graph)
+        self.btn_redraw.place(x=20, rely=0.9)
 
         self.frame2 = tk.Frame(self, bg=self.cget('bg'))
         self.frame2.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
@@ -41,7 +36,26 @@ class GraphGUI(ctk.CTkToplevel):
         self.lbl_path = ctk.CTkLabel(self.frame2, text=config.TEXT_BODY_RESULT_FLOW)
         self.lbl_path.pack(pady=7, padx=10)
 
-        ford_falkerson = nx.maximum_flow(G, config.TEXT_SOURCE, config.TEXT_STOCK)
+        ford_falkerson = nx.maximum_flow(self.G, config.TEXT_SOURCE, config.TEXT_STOCK)
 
         self.lbl_flow.configure(text=config.TEXT_BODY_RESULT_FLOW + f': {ford_falkerson[0]}')
         self.lbl_path.configure(text=config.TEXT_BODY_RESULT_PATH + f': {ford_falkerson[1]}')
+    
+    def draw_graph(self):
+        self.fig.clear()
+        self.ax = self.fig.add_subplot(111)
+        self.G, self.pos = self.__create_graph()
+        nx.draw(self.G, self.pos, ax=self.ax, with_labels=True, node_size=700, node_color="skyblue", font_size=10,
+                font_color="black", edge_color='red')
+
+        nx.draw_networkx_edge_labels(self.G, self.pos, edge_labels={key: value for key, value in zip(self.data[0], self.data[1])})
+        self.ax.set_title(config.TEXT_APP_TITLE)
+        self.canvas.draw()
+
+    
+    def __create_graph(self):
+        G = nx.DiGraph()
+        G.add_edges_from([(edge[0], edge[1], {'capacity': capacity}) for edge, capacity in zip(self.data[0], self.data[1])])
+        pos = nx.spring_layout(G, k=len(self.data[1]))
+        return G, pos
+
--- main.py
+++ main.py
@@ -25,30 +25,32 @@ class MainGUI(ctk.CTk, LogMixin):
             self.columnconfigure(ind, weight=1)
 
         self.menu_frame = tk.Frame(self, bg=self.cget('bg'))
-        self.menu_frame.grid(row=0, columnspan=3, sticky="nsew")
+        self.menu_frame.grid(row=0, columnspan=3, sticky="nsew", ipady=10)
         self.help_btn = ctk.CTkButton(self.menu_frame, text=config.TEXT_HELP, fg_color=self.cget('bg'), command=self.show_help, **config.BTN_HELP_STYLE_DICT)
-        self.help_btn.pack(anchor='nw', ipadx=6)
+        self.help_btn.pack(anchor='nw', ipadx=6, side='left')
+        # TODO: make a generate_btn click event that generates data for graph in GUI
+        self.generate_btn = ctk.CTkButton(self.menu_frame, text=config.TEXT_BTN_GENERATE, fg_color=self.cget('bg'), command=self.show_help, **config.BTN_HELP_STYLE_DICT)
+        self.generate_btn.pack(anchor='nw', ipadx=6, side='left')
 
         self.title_lbl = ctk.CTkLabel(self, text=config.TEXT_BODY_TITLE, font=('Arial', 22))
         self.title_lbl.grid(columnspan=3, column=0, row=1, pady=15)
 
-        # Separate window for fields
-        self.canvas = tk.Canvas(self, highlightthickness=0, bg=self.cget('bg'))
-        self.canvas.grid(column=0, columnspan=3, row=2)
+        self.canvas = tk.Canvas(self, highlightthickness=0, bg=config.CANVAS_COLOR)
+        self.canvas.grid(column=0, columnspan=3, row=2, sticky='ew')
         self.update()
 
         self.scrollbar = ctk.CTkScrollbar(self, command=self.canvas.yview)
         self.scrollbar.grid(sticky='nse', column=3, row=2)
         self.canvas.configure(yscrollcommand=self.scrollbar.set)
-        self.frame = tk.Frame(self.canvas, padx=2, bg=self.cget('bg'))
-        self.canvas.create_window(0, 0, window=self.frame, anchor=tk.NW, width=self.canvas.winfo_width())
+        self.frame = tk.Frame(self.canvas, padx=2, bg=config.CANVAS_COLOR)
+        self.canvas.create_window(0, 0, window=self.frame, anchor=tk.NW, width=620)
         self.update_canvas()
 
         temp_frame = self.create_temp_frame()
         self.origin_lbl = ctk.CTkLabel(temp_frame, text=config.TEXT_SOURCE_LBL)
-        self.origin_lbl.grid(column=0, row=3)
+        self.origin_lbl.grid(column=0, row=3, columnspan=2)
         self.origin_entry = ctk.CTkEntry(temp_frame)
-        self.origin_entry.grid(column=1, row=3, **config.ENTRY_STYLE_DICT)
+        self.origin_entry.grid(column=2, row=3, **config.ENTRY_STYLE_DICT_ORIGIN)
 
         self.add_vertex_btn = ctk.CTkButton(self, text=config.TEXT_ADD_VERTEX, command=self.add_vertex_gui)
         self.add_vertex_btn.grid(column=0, row=3, **config.BTN_STYLE_DICT)
@@ -67,27 +69,32 @@ class MainGUI(ctk.CTk, LogMixin):
         items = self.__raw_data.pop()
         for item in items:
             item.destroy()
-        VertexNameGenerator.decrease_counter()
         self.update_canvas()
         self.logger.info('Widget deleted')
 
     def create_temp_frame(self):
-        temp_frame = tk.Frame(self.frame, bg=self.cget('bg'))
-        for ind in range(3):
+        temp_frame = tk.Frame(self.frame, bg=config.CANVAS_COLOR)
+        for ind in range(6):
             temp_frame.columnconfigure(ind, weight=1)
         temp_frame.pack(expand=True, side=tk.TOP, fill='x')
         return temp_frame
 
     def add_vertex_gui(self):
         temp_frame = self.create_temp_frame()
-        lbl = ctk.CTkLabel(temp_frame, text=VertexNameGenerator.get_vertex_name(), width=5)
+        lbl = ctk.CTkLabel(temp_frame, text=config.TEXT_VERTEX, width=5)
         lbl.grid(column=0, row=0)
-        entry = ctk.CTkEntry(temp_frame)
+        entry = ctk.CTkEntry(temp_frame, width=1)
         entry.grid(column=1, row=0, **config.ENTRY_STYLE_DICT)
 
-        self.__raw_data.append([lbl, entry, temp_frame])
+        lbl = ctk.CTkLabel(temp_frame, text=config.TEXT_DI, width=5)
+        lbl.grid(column=3, row=0)
+
+        entry_di = ctk.CTkEntry(temp_frame)
+        entry_di.grid(column=4, row=0, **config.ENTRY_STYLE_DICT)
+
+        self.__raw_data.append([entry, entry_di, temp_frame])
         self.update_canvas()
-        self.logger.info(f'Widget added with name {lbl.cget("text")}')
+        self.logger.info(f'Widget added')
 
     def update_canvas(self):
         self.canvas.update_idletasks()
@@ -95,8 +102,12 @@ class MainGUI(ctk.CTk, LogMixin):
 
     def prepare_to_graph(self):
         new = list(map(lambda item: item[:-1], self.__raw_data))
-        new.append([ttk.Label(text=config.TEXT_SOURCE), self.origin_entry])
-        new.append([ttk.Label(text=config.TEXT_STOCK), ttk.Entry()])
+        temp_entry = ctk.CTkEntry(self)
+        temp_entry.insert(-1, config.TEXT_SOURCE)
+        new.append([temp_entry, self.origin_entry])
+        temp_entry = ctk.CTkEntry(self)
+        temp_entry.insert(-1, config.TEXT_STOCK)
+        new.append([temp_entry, ttk.Entry()])
         result = DataConverter().widget_to_json(new)
         if not result:
             self.logger.warning('DataConverter returned empty data')
